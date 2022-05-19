da56940fa491b9b68c1bc497bcf76e97d2a4e492
Morteza Jamshidi <devmor.j@gmail.com>
2022-05-19 22:45:49 +0430

    Drop postcss pipeline and optimize html and images

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,42 @@
 const { defineConfig } = require("vite");
+import { createHtmlPlugin } from "vite-plugin-html";
+import viteImagemin from 'vite-plugin-imagemin'
 
 export default defineConfig({
   base: './',
   build: {
     outDir: 'docs'
-  }
+  },
+  plugins: [
+    createHtmlPlugin({
+      minify: true,
+    }),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+    }),
+  ]
 })
